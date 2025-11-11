74027e1e7bd67f9072d9905355f25e952d00e394
Blane Cummings <blanecummings@hotmail.com>
2025-11-11 10:44:29 -0600

    day5 part two

--- 2015/05/solution.py
+++ 2015/05/solution.py
@@ -1,20 +1,46 @@
-with open("/home/bcummings/repos/advent-of-code/2015/05/input.txt", "r") as f:
-    strings = f.read().splitlines()
-
-nice_strings = 0
-vowels = ["a", "e", "i", "o", "u"]
-forbidden = ["ab", "cd", "pq", "xy"]
+import re
 
 
-for s in strings:
-    num_vowels = sum(1 for ch in s if ch in vowels)
-    has_repeating = any(s[i] == s[i + 1] for i in range(len(s) - 1))
-    has_forbidden = any(f in s for f in forbidden)
+def is_nice1(s):
+    # contains at least three vowels (a, e, i, o, u)?
+    has_3_vowels = sum(c in "aeiou" for c in s) >= 3
 
-    if num_vowels >= 3 and has_repeating and not has_forbidden:
-        nice_strings += 1
+    # has a letter that appears twice in a row?
+    has_double = re.search(r"(.)\1", s) is not None
 
-    log = f"{s}: vowels={num_vowels}, has_repeating={has_repeating}, has_forbidden={has_forbidden}"
-    print(log)
+    # does not contain (ab, cd, pq, or xy)?
+    has_forbidden = re.search(r"ab|cd|pq|xy", s) is not None
 
-print(f"Nice strings = {nice_strings}")
+    return has_3_vowels and has_double and not has_forbidden
+
+
+def is_nice2(s):
+    # has a pair of any two letters that appear twice without overlapping?
+    has_pair = re.search(r"(..).*\1", s) is not None
+
+    # has a letter that repeats with exactly one letter between them?
+    has_repeat = re.search(r"(.).\1", s) is not None
+
+    return has_pair and has_repeat
+
+
+def main():
+    with open("/home/bcummings/repos/advent-of-code/2015/05/input.txt", "r") as f:
+        strings = f.read().splitlines()
+
+    num_nice_strings_p1 = 0
+    num_nice_strings_p2 = 0
+
+    for s in strings:
+        if is_nice1(s):
+            num_nice_strings_p1 += 1
+
+        if is_nice2(s):
+            num_nice_strings_p2 += 1
+
+    print(f"Nice strings (part1) = {num_nice_strings_p1}")
+    print(f"Nice strings (part2) = {num_nice_strings_p2}")
+
+
+if __name__ == "__main__":
+    main()
